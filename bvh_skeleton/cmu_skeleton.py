2b30ec84aeb3d4a58753f8414e16dcb4f7425438
hajiarab1988 <39706772+hajiarab1988@users.noreply.github.com>
2024-08-15 16:09:36 +0330

    Update cmu_skeleton.py

--- bvh_skeleton/cmu_skeleton.py
+++ bvh_skeleton/cmu_skeleton.py
@@ -17,13 +17,16 @@ class CMUSkeleton(object):
             'LeftUpLeg': 4,
             'LeftLeg': 5,
             'LeftFoot': 6,
-            'Neck': 7,
-            'LeftArm': 8,
-            'LeftForeArm': 9,
-            'LeftHand': 10,
-            'RightArm': 11,
-            'RightForeArm': 12,
-            'RightHand': 13,
+            'Spine': 7,
+            'Spine1': 8,
+            'Neck1': 9,
+            'HeadEndSite': 10,
+            'LeftArm': 11,
+            'LeftForeArm': 12,
+            'LeftHand': 13,
+            'RightArm': 14,
+            'RightForeArm': 15,
+            'RightHand': 16,
             'RightHipJoint': -1,
             'RightFootEndSite': -1,
             'LeftHipJoint': -1,
@@ -33,30 +36,39 @@ class CMUSkeleton(object):
             'RightShoulder': -1,
             'RightHandEndSite': -1,
             'LowerBack': -1,
-            'Spine': -1,
-            'Spine1': -1,
-            'HeadEndSite': -1
+            'Neck': -1
         }
         self.index2keypoint = {v: k for k, v in self.keypoint2index.items()}
         self.keypoint_num = len(self.keypoint2index)
 
-        # Define the new hierarchy without Spine, Spine1, and HeadEndSite
         self.children = {
             'Hips': ['LeftHipJoint', 'LowerBack', 'RightHipJoint'],
             'LeftHipJoint': ['LeftUpLeg'],
             'LeftUpLeg': ['LeftLeg'],
             'LeftLeg': ['LeftFoot'],
             'LeftFoot': ['LeftFootEndSite'],
-            'LowerBack': ['Neck'],
-            'Neck': ['LeftArm', 'RightArm'],
+            'LeftFootEndSite': [],
+            'LowerBack': ['Spine'],
+            'Spine': ['Spine1'],
+            'Spine1': ['LeftShoulder', 'Neck', 'RightShoulder'],
+            'LeftShoulder': ['LeftArm'],
             'LeftArm': ['LeftForeArm'],
             'LeftForeArm': ['LeftHand'],
+            'LeftHand': ['LeftHandEndSite'],
+            'LeftHandEndSite': [],
+            'Neck': ['Neck1'],
+            'Neck1': ['HeadEndSite'],
+            'HeadEndSite': [],
+            'RightShoulder': ['RightArm'],
             'RightArm': ['RightForeArm'],
             'RightForeArm': ['RightHand'],
+            'RightHand': ['RightHandEndSite'],
+            'RightHandEndSite': [],
             'RightHipJoint': ['RightUpLeg'],
             'RightUpLeg': ['RightLeg'],
             'RightLeg': ['RightFoot'],
-            'RightFoot': ['RightFootEndSite']
+            'RightFoot': ['RightFootEndSite'],
+            'RightFootEndSite': [],
         }
         self.parent = {self.root: None}
         for parent, children in self.children.items():
@@ -72,6 +84,7 @@ class CMUSkeleton(object):
             if 'Right' in joint
         ]
 
+        # T-pose
         self.initial_directions = {
             'Hips': [0, 0, 0],
             'LeftHipJoint': [1, 0, 0],
@@ -79,13 +92,22 @@ class CMUSkeleton(object):
             'LeftLeg': [0, 0, -1],
             'LeftFoot': [0, 0, -1],
             'LeftFootEndSite': [0, -1, 0],
-            'Neck': [0, 0, 1],
+            'LowerBack': [0, 0, 1],
+            'Spine': [0, 0, 1],
+            'Spine1': [0, 0, 1],
+            'LeftShoulder': [1, 0, 0],
             'LeftArm': [1, 0, 0],
             'LeftForeArm': [1, 0, 0],
             'LeftHand': [1, 0, 0],
+            'LeftHandEndSite': [1, 0, 0],
+            'Neck': [0, 0, 1],
+            'Neck1': [0, 0, 1],
+            'HeadEndSite': [0, 0, 1],
+            'RightShoulder': [-1, 0, 0],
             'RightArm': [-1, 0, 0],
             'RightForeArm': [-1, 0, 0],
             'RightHand': [-1, 0, 0],
+            'RightHandEndSite': [-1, 0, 0],
             'RightHipJoint': [-1, 0, 0],
             'RightUpLeg': [-1, 0, 0],
             'RightLeg': [0, 0, -1],
@@ -167,73 +189,90 @@ class CMUSkeleton(object):
         while stack:
             node = stack.pop()
             joint = node.name
-            joint_idx = self.keypoint2index.get(joint, -1)
+            joint_idx = self.keypoint2index[joint]
             
-            if joint_idx == -1:
-                continue  # Skip joints not present in the pose data
-    
             if node.is_root:
                 channel.extend(pose[joint_idx])
-    
+
+            index = self.keypoint2index
             order = None
             if joint == 'Hips':
-                x_dir = pose[self.keypoint2index['LeftUpLeg']] - pose[self.keypoint2index['RightUpLeg']]
-                z_dir = pose[self.keypoint2index['Neck']] - pose[joint_idx]
+                x_dir = pose[index['LeftUpLeg']] - pose[index['RightUpLeg']]
+                y_dir = None
+                z_dir = pose[index['Spine']] - pose[joint_idx]
                 order = 'zyx'
             elif joint in ['RightUpLeg', 'RightLeg']:
                 child_idx = self.keypoint2index[node.children[0].name]
-                x_dir = pose[self.keypoint2index['Hips']] - pose[self.keypoint2index['RightUpLeg']]
+                x_dir = pose[index['Hips']] - pose[index['RightUpLeg']]
+                y_dir = None
                 z_dir = pose[joint_idx] - pose[child_idx]
                 order = 'zyx'
             elif joint in ['LeftUpLeg', 'LeftLeg']:
                 child_idx = self.keypoint2index[node.children[0].name]
-                x_dir = pose[self.keypoint2index['LeftUpLeg']] - pose[self.keypoint2index['Hips']]
+                x_dir = pose[index['LeftUpLeg']] - pose[index['Hips']]
+                y_dir = None
                 z_dir = pose[joint_idx] - pose[child_idx]
                 order = 'zyx'
-            elif joint == 'Neck':
-                x_dir = pose[self.keypoint2index['LeftArm']] - pose[self.keypoint2index['RightArm']]
-                z_dir = pose[joint_idx] - pose[self.keypoint2index['Neck']]
+            elif joint == 'Spine':
+                x_dir = pose[index['LeftUpLeg']] - pose[index['RightUpLeg']]
+                y_dir = None
+                z_dir = pose[index['Spine1']] - pose[joint_idx]
                 order = 'zyx'
+            elif joint == 'Spine1':
+                x_dir = pose[index['LeftArm']] - \
+                    pose[index['RightArm']]
+                y_dir = None
+                z_dir = pose[joint_idx] - pose[index['Spine']]
+                order = 'zyx'
+            elif joint == 'Neck1':
+                x_dir = None
+                y_dir = pose[index['Spine1']] - pose[joint_idx]
+                z_dir = pose[index['HeadEndSite']] - pose[index['Spine1']]
+                order = 'zxy'
             elif joint == 'LeftArm':
-                x_dir = pose[self.keypoint2index['LeftForeArm']] - pose[joint_idx]
-                z_dir = pose[joint_idx] - pose[self.keypoint2index['LeftHand']]
+                x_dir = pose[index['LeftForeArm']] - pose[joint_idx]
+                y_dir = pose[index['LeftForeArm']] - pose[index['LeftHand']]
+                z_dir = None
                 order = 'xzy'
             elif joint == 'LeftForeArm':
-                x_dir = pose[self.keypoint2index['LeftHand']] - pose[joint_idx]
-                z_dir = pose[joint_idx] - pose[self.keypoint2index['LeftArm']]
+                x_dir = pose[index['LeftHand']] - pose[joint_idx]
+                y_dir = pose[joint_idx] - pose[index['LeftArm']]
+                z_dir = None
                 order = 'xzy'
             elif joint == 'RightArm':
-                x_dir = pose[joint_idx] - pose[self.keypoint2index['RightForeArm']]
-                z_dir = pose[self.keypoint2index['RightForeArm']] - pose[self.keypoint2index['RightHand']]
+                x_dir = pose[joint_idx] - pose[index['RightForeArm']]
+                y_dir = pose[index['RightForeArm']] - pose[index['RightHand']]
+                z_dir = None
                 order = 'xzy'
             elif joint == 'RightForeArm':
-                x_dir = pose[joint_idx] - pose[self.keypoint2index['RightHand']]
-                z_dir = pose[joint_idx] - pose[self.keypoint2index['RightArm']]
+                x_dir = pose[joint_idx] - pose[index['RightHand']]
+                y_dir = pose[joint_idx] - pose[index['RightArm']]
+                z_dir = None
                 order = 'xzy'
             
             if order:
-                dcm = math3d.dcm_from_axis(x_dir, None, z_dir, order)
+                dcm = math3d.dcm_from_axis(x_dir, y_dir, z_dir, order)
                 quats[joint] = math3d.dcm2quat(dcm)
             else:
-                quats[joint] = quats.get(self.parent.get(joint, None), None)
-    
-            local_quat = quats.get(joint, None)
-            if local_quat is not None and node.parent:
+                quats[joint] = quats[self.parent[joint]].copy()
+            
+            local_quat = quats[joint].copy()
+            if node.parent:
                 local_quat = math3d.quat_divide(
                     q=quats[joint], r=quats[node.parent.name]
                 )
-    
+            
             euler = math3d.quat2euler(
                 q=local_quat, order=node.rotation_order
             )
             euler = np.rad2deg(euler)
             eulers[joint] = euler
             channel.extend(euler)
-    
+
             for child in node.children[::-1]:
                 if not child.is_end_site:
                     stack.append(child)
-    
+
         return channel
 
 
